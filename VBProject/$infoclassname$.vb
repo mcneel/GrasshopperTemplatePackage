30e38d50110368044272dd999fc86dc9d120c0fc
David Rutten <david@mcneel.com>
2013-09-18 11:23:13 +0200

    Updated Assembly info class

--- VBProject/$infoclassname$.vb
+++ VBProject/$infoclassname$.vb
@@ -8,7 +8,34 @@ Public Class $infoclassname$
 			Return "$infovisualname$"
 		End Get
 	End Property
+	Public Overrides ReadOnly Property Icon As System.Drawing.Bitmap
+		Get
+			'Return a 24x24 pixel bitmap to represent this GHA library.
+			Return Nothing
+		End Get
+	End Property
+	Public Overrides ReadOnly Property Description As String
+		Get
+			'Return a short string describing the purpose of this GHA library.
+			Return ""
+		End Get
+	End Property
+	Public Overrides ReadOnly Property Id As System.Guid
+		Get
+			Return New System.Guid("$guid9$")
+		End Get
+	End Property
 
-	'Override here any more methods you see fit.
-	'Start typing Overrides..., select a property and push Enter.
+	Public Overrides ReadOnly Property AuthorName As String
+		Get
+			'Return a string identifying you or your company.
+			Return MyBase.AuthorName
+		End Get
+	End Property
+	Public Overrides ReadOnly Property AuthorContact As String
+		Get
+			'Return a string representing your preferred contact details.
+			Return MyBase.AuthorContact
+		End Get
+	End Property
 End Class
